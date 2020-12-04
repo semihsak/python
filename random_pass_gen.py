05f151976517af66ba8c6d644efb976be78e7b9b
semih <semihsak@gmail.com>
2020-12-04 12:51:31 +0300

    some lines added

--- random_pass_gen.py
+++ random_pass_gen.py
@@ -2,6 +2,7 @@
 # This script generate password how much you want.
 # License MIT
 
+from sys import exit
 import random
 
 gpass = []
@@ -15,6 +16,14 @@ all = low + up + numbers + symbols
 
 length = int(input("How much characters made your password : "))
 pieces = int(input("How much times of password : "))
+
+if length > 70 and pieces > 0:
+    raise ValueError("Value for length is too large")
+elif length < 0 or pieces < 0:
+    raise ValueError("Value length or pieces cannot be negative")
+elif length == 0 or pieces == 0:
+    print("length or pieces cannot be zero")
+    exit(1)
 
 for i in range(pieces):
     gpass.append("".join(random.sample(all,length)))
